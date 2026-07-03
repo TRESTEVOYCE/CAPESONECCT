ea4dd40f4049f07782553da18ca76032b2b3a5c1
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-07-03 22:39:11 +0800

    registered models to admin.py

--- AdminSide/admin.py
+++ AdminSide/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . models import User, EmployerProfile, PESOActivities, AuditLog, Jobs, ApplicantProfile, ApplicantSkills, AppliedJobs, OfferedJobs, GovernmentInternshipProgram, TupadBeneficiary, DisplacedInformalLaborProgram, SpecialProgramForEmploymentOfStudents
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(EmployerProfile)
+admin.site.register(PESOActivities)
+admin.site.register(AuditLog)
+admin.site.register(Jobs)
+admin.site.register(ApplicantProfile)
+admin.site.register(ApplicantSkills)
+admin.site.register(AppliedJobs)
+admin.site.register(OfferedJobs)
+admin.site.register(GovernmentInternshipProgram)
+admin.site.register(TupadBeneficiary)
+admin.site.register(DisplacedInformalLaborProgram)
+admin.site.register(SpecialProgramForEmploymentOfStudents)
+admin.site.register(PESOActivities)
+admin.site.register(AuditLog)
