070ffe98a31d55808bc1c5a1ecb8bd31d891d463
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-07-18 12:26:01 +0800

    created api views for the informations to be accessed by the MSWD

--- MSWDAPI/views.py
+++ MSWDAPI/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView,RetrieveAPIView
+from .serializers import SpecialProgramSerializer
+from AdminSide.models import SpecialProgramBeneficiaries
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class SpecialProgramAPI(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = SpecialProgramBeneficiaries.objects.all
+    serializer_class = SpecialProgramSerializer
+
