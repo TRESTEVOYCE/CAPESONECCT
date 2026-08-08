bc43d86ea570e7f2b5287dec52bf3570b88284b7
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-08-08 23:32:20 +0800

    added a new endpoint for DRF browsable API login

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AdminSide.urls')),    
     path('api/',include('MSWDAPI.urls')),
+    path('api-auth/', include('rest_framework.urls')),
      
 ]
