ff0bafc72a4752762e9c7d32f56208d0a62c86ed
Ann Trecia Balendo <anntrecia.balendo@evsu.edu.ph>
2026-09-03 13:42:48 +0800

    Fix Merging conflicts.

--- config/urls.py
+++ config/urls.py
@@ -21,8 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('LandingPage.urls')),
     path('employer/', include('EmployerSide.urls')),
-    path('admin-side/', include('AdminSide.urls')),    
-    path('', include(('AdminSide.urls'), namespace='admin_side')),    
+    path('admin-side/', include('AdminSide.urls')),       
     path('api/',include('MSWDAPI.urls')),
     path('api-auth/', include('rest_framework.urls')),  
 ]
