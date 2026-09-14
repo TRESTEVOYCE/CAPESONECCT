b7d19cc1ee1baebf63240bf9f8124d83f3a024cb
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-09-14 13:18:19 +0800

    updated the urls to add functionality in the system such as search and delete

--- ApplicantSide/urls.py
+++ ApplicantSide/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
 
     path('profile/delete/', ApplicantProfileDeleteView.as_view(), name='profile_delete'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('dashboard/', DashBoardView.as_view(), name='dashboard'),
+    path('applicant-dashboard/', DashBoardView.as_view(), name='applicant-dashboard'),
 
     path('jobs/', JobListView.as_view(), name='job_list'),
     path('jobs/sort/', SortJobView.as_view(), name='sort_jobs'),
