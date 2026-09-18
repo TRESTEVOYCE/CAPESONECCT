5fd45c831ca59a1cb8ce3f7acb15972fb3f2bfa1
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-09-18 15:39:56 +0800

    updated the forms

--- EmployerSide/forms.py
+++ EmployerSide/forms.py
@@ -1,13 +1,29 @@
-from django import forms 
-from AdminSide.models import EmployerProfile,Jobs
+from django import forms
+from AdminSide.models import EmployerProfile, Jobs
 
 
 class EmployerProfileForm(forms.ModelForm):
     class Meta:
         model = EmployerProfile
-        fields = '__all__'
+        fields = [
+            'business_name', 'trade_name', 'acronym', 'office_type',
+            'tin_number', 'employer_type', 'total_workforce', 'line_of_business',
+            'street_address', 'barangay', 'municipality', 'province',
+            'owner_name', 'designation', 'contact_person', 'contact_position',
+            'telephone_number', 'mobile_number', 'email',
+            'certificate_of_registration', 'dti_sec_registration', 'business_permit',
+            'public_doc_type', 'public_verification_document',
+        ]
+
 
 class JobsForm(forms.ModelForm):
     class Meta:
         model = Jobs
-        fields = '__all__'
+        fields = [
+            'job_title', 'job_description', 'nature_of_work', 'place_of_work',
+            'salary', 'vacancy', 'work_experience_months', 'other_qualifications',
+            'accepts_pwd', 'pwd_disabilities', 'accepts_ofw',
+            'educational_level', 'course_or_strand', 'required_license',
+            'required_eligibility', 'required_certification', 'languages_spoken',
+            'application_quota', 'job_posting_expiry',
+        ]
