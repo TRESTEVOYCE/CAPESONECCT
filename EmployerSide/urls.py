0aeae8ffc5fa64e4c1849955d53d9234ccec5608
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-09-21 09:28:38 +0800

    added a new urls for the new pages and the a update url for updating a profile pictures

--- EmployerSide/urls.py
+++ EmployerSide/urls.py
@@ -1,8 +1,9 @@
 from django import urls
 from django.urls import path
-from .views import EmployerProfileCreateView, HomeView, ApplicantsListView, ApplicantDetailView, JobCreationView, JobUpdateView, JobDeleteView,JobDetailView, AccountDeleteView,ApplicantJobStatusView,ApplicantJobStatusView,LogoutView
+from .views import EmployerProfileCreateView, HomeView, ApplicantsListView, ApplicantDetailView, JobCreationView, JobUpdateView, JobDeleteView,JobDetailView, AccountDeleteView,ApplicantJobStatusView,ApplicantJobStatusView,LogoutView,JobListView,CompanyProfileView,EmployerProfilePictureView
 
 urlpatterns = [
+    path('company_profile/',CompanyProfileView.as_view(),name='company_profile_view'),
     path('employer-profile/create/', EmployerProfileCreateView.as_view(), name='employer-profile-create'),
     path('home/', HomeView.as_view(), name='employer-home'),
     path('applicants/', ApplicantsListView.as_view(), name='applicants-list'),
@@ -15,5 +16,7 @@ urlpatterns = [
     path('applicants/<int:pk>/job-status/', ApplicantJobStatusView.as_view(), name='applicant-job-status'),
     path('applicants/<int:pk>/job-status/update/', ApplicantJobStatusView.as_view(), name='applicant-job-status-update'),
     path('logout/',LogoutView.as_view(), name='employer-logout'),
+    path('jobs_posted/',JobListView.as_view(),name="employer-job_list"),
+    path('employer-profile/picture/',EmployerProfilePictureView.as_view(),name='employer-profile-picture'),
 
 ]
