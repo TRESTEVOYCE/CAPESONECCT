aa7761aa5742a866d599ef4704672a43562cb8e1
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-09-21 09:33:53 +0800

    updated views for the new form for the two step registration

--- LandingPage/views.py
+++ LandingPage/views.py
@@ -1,46 +1,99 @@
-from django.views.generic import CreateView, FormView, TemplateView
+from django.views.generic import FormView, TemplateView
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from honeypot.decorators import check_honeypot
 from django.utils.decorators import method_decorator
-from AdminSide.models import EmployerProfile, User
-from .forms import UserRegisterForm, SignInForm
+from AdminSide.models import ApplicantProfile,EmployerProfile
+from .forms import UserRegisterForm,SignInForm,BasicApplicantInformationForms,BasicEmployerInformationForms
 
 
 class Register_SelectionView(TemplateView):
     template_name = 'LandingPage/register_selection.html'
 
+# APPLICANT REGISTRATION
 @method_decorator(check_honeypot, name='dispatch')
-class UserRegisterView(CreateView):
-    model = User
-    form_class = UserRegisterForm
-    user_role = None
+class UserApplicantRegisterView(FormView):
+    template_name = 'LandingPage/register_jobseeker.html'
+    form_class = BasicApplicantInformationForms
 
     def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.role = self.user_role
-        self.object.save()
+        self.request.session['applicant_info'] = {
+            **form.cleaned_data,
+            'date_of_birth': form.cleaned_data['date_of_birth'].isoformat(),
+        }
+
+        return redirect('applicant-account-register')
+
+
+@method_decorator(check_honeypot, name='dispatch')
+class UserApplicantAccountRegisterView(FormView):
+    template_name = 'LandingPage/register_jobseeker_account.html'
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('applicant-dashboard')
+
+    def form_valid(self, form):
+        info = self.request.session.get('applicant_info')
+
+        if not info:
+            return redirect('register_jobseeker')
+
+        user = form.save(commit=False)
+        user.role = 'applicant'
+        user.save()
+
+        ApplicantProfile.objects.create(
+            user=user,
+            **info
+        )
+
+        del self.request.session['applicant_info']
+
+        login(self.request, user)
 
         return redirect(self.success_url)
 
-class UserApplicantRegisterView(UserRegisterView):
-    template_name = 'LandingPage/register_jobseeker.html'
-    user_role = 'applicant'
-    success_url = reverse_lazy('applicant-dashboard') 
 
-class UserEmployerRegisterView(UserRegisterView):
+# EMPLOYER REGISTRATION
+@method_decorator(check_honeypot, name='dispatch')
+class UserEmployerRegisterView(FormView):
     template_name = 'LandingPage/register_employer.html'
-    user_role = 'employer'
+    form_class = BasicEmployerInformationForms
+
+    def form_valid(self, form):
+        self.request.session['employer_info'] = form.cleaned_data
+
+        return redirect('employer-account-register')
+
+
+@method_decorator(check_honeypot, name='dispatch')
+class UserEmployerAccountRegisterView(FormView):
+    template_name = 'LandingPage/register_employer_account.html'
+    form_class = UserRegisterForm
     success_url = reverse_lazy('employer-home')
 
     def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.role = self.user_role
-        self.object.save()
-        EmployerProfile.objects.get_or_create(user=self.object)
+        info = self.request.session.get('employer_info')
+
+        if not info:
+            return redirect('register_employer')
+
+        user = form.save(commit=False)
+        user.role = 'employer'
+        user.save()
+
+        EmployerProfile.objects.update_or_create(
+            user=user,
+            defaults=info
+        )
+
+        del self.request.session['employer_info']
+
+        login(self.request, user)
+
         return redirect(self.success_url)
 
+# SIGN IN
 @method_decorator(check_honeypot, name='dispatch')
 class SignInView(FormView):
     form_class = SignInForm
@@ -54,7 +107,12 @@ class SignInView(FormView):
         print("Email:", email)
         print("Password:", password)
 
-        user = authenticate(self.request, email=email, password=password)
+        user = authenticate(
+            self.request,
+            email=email,
+            password=password
+        )
+
         if not user:
             form.add_error(
                 None,
@@ -64,7 +122,6 @@ class SignInView(FormView):
 
         print("Authenticated user:", user.username)
         print("Database role:", user.role)
-
 
         login(self.request, user)
 
@@ -83,6 +140,6 @@ class SignInView(FormView):
 
         return super().form_invalid(form)
 
+# LANDING PAGE
 class LandingPageView(TemplateView):
     template_name = 'LandingPage/index.html'
-
