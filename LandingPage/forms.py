e5005eb45751d0a1bf9ab256ab187dba88b11f54
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-09-21 09:32:35 +0800

    updated forms to cater the two step sign up form

--- LandingPage/forms.py
+++ LandingPage/forms.py
@@ -1,4 +1,4 @@
-from AdminSide.models import User
+from AdminSide.models import User,ApplicantProfile,EmployerProfile
 from django.forms import ModelForm
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
@@ -7,6 +7,21 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class BasicApplicantInformationForms(ModelForm):
+    class Meta:
+        model = ApplicantProfile
+        fields = ['first_name','middle_name','last_name','sex','phone_number']
+
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+
+class BasicEmployerInformationForms(ModelForm):
+    class Meta:
+        model = EmployerProfile
+        fields = ['business_name','trade_name','mobile_number','owner_name','email']
 
 class SignInForm(forms.Form):
     email = forms.EmailField()
